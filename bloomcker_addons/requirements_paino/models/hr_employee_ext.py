fc42d181309005b8ae49de477ec80fa8ab61eb27
Luis Millan <lmillan131@gmail.com>
2021-05-06 13:23:36 -0400

    correcciones

--- bloomcker_addons/requirements_paino/models/hr_employee_ext.py
+++ bloomcker_addons/requirements_paino/models/hr_employee_ext.py
@@ -17,12 +17,12 @@ class EmployeeExt(models.Model):
 	bank_account_cts_id = fields.Many2one('res.partner.bank', string='Cuenta CTS',
 		domain="[('partner_id', '=', address_home_id)]", help='Cuenta CTS')
 
-	def get_view_bank(self):
-        return {
-            'domain' : filtro,
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move.line.book',
-            'view_mode': 'tree',
-            'view_type': 'form',
-            'views': [(False, 'tree')],
-        }
+	# def get_view_bank(self):
+    #     return {
+    #         'domain' : filtro,
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'account.move.line.book',
+    #         'view_mode': 'tree',
+    #         'view_type': 'form',
+    #         'views': [(False, 'tree')],
+    #     }
