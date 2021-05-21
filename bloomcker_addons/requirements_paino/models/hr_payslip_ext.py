42c5faa87e74071a2b421dc531b1bd7e14d277fd
Luis Millan <lmillan131@gmail.com>
2021-05-21 08:32:10 -0400

    boleta xml

--- bloomcker_addons/requirements_paino/models/hr_payslip_ext.py
+++ bloomcker_addons/requirements_paino/models/hr_payslip_ext.py
@@ -1,21 +1,8 @@
-import time
-from datetime import datetime, timedelta
-from dateutil import relativedelta
-from xml.etree.ElementTree import ElementTree
-import babel
-
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.safe_eval import safe_eval
-from reportlab.lib.pagesizes import letter, A4
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER, TA_RIGHT, TA_LEFT
-from decimal import *
-from math import modf
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import logging
 _logger = logging.getLogger(__name__)
-
 
 class HrPayslipExtend(models.Model):
 	_inherit = 'hr.payslip'
@@ -25,98 +12,63 @@ class HrPayslipExtend(models.Model):
 	def imprimir_boleta_xml(self):
 		_logger.info('imprimir_xml')
 		self.ensure_one()
-		dias_no_laborados,dias_laborados,first,second,dias_faltas = 0,0,0,0,0
-		payslips = self.env['hr.payslip'].search([('payslip_run_id','=',self.payslip_run_id.id),('employee_id','=',self.employee_id.id)])
-		planilla_ajustes = self.env['planilla.ajustes'].search([], limit=1)
-		try:
-			ruta = self.env['main.parameter.hr'].search([])[0].dir_create_file
-		except:
-			raise UserError('Falta configurar un directorio de descargas en el menu Configuracion/Parametros/Directorio de Descarga')
-		archivo_pdf = SimpleDocTemplate(
-			ruta+"planilla_tmp.xml", pagesize=A4, rightMargin=30, leftMargin=30, topMargin=30, bottomMargin=20)
 
-		elements = []
-		company = self.env['res.company'].search([], limit=1)
-		categories = self.env['hr.salary.rule.category'].search(
-			[('aparece_en_nomina', '=', True)], order="secuencia")
+		records = Element('records')
+		record = SubElement(records, 'record')
+		ciudad_fecha = SubElement(record, 'ciudad_fecha')
+		datos_empleador = SubElement(record, 'datos_empleador')
+		de = SubElement(record, 'de')
+		nombres_apellidos = SubElement(record, 'nombres_apellidos')
+		cargo = SubElement(record, 'cargo')
+		fecha_ingreso = SubElement(record, 'fecha_ingreso')
+		entidad_depositaria = SubElement(record, 'entidad_depositaria')
+		cuenta_soles = SubElement(record, 'cuenta_soles')
+		cuenta_dolares = SubElement(record, 'cuenta_dolares')
+		periodo_deposito_liquidar_del = SubElement(record, 'periodo_deposito_liquidar_del')
+		periodo_deposito_liquidar_al = SubElement(record, 'periodo_deposito_liquidar_al')
+		periodo_depositar_del = SubElement(record, 'periodo_depositar_del')
+		periodo_depositar_al = SubElement(record, 'periodo_depositar_al')
+		mes = SubElement(record, 'mes')
+		dia = SubElement(record, 'dia')
+		calculo_meses_laborados = SubElement(record, 'calculo_meses_laborados')
+		calculo_dias_laborados = SubElement(record, 'calculo_dias_laborados')
+		importe_calculado = SubElement(record, 'importe_calculado')
+		total = SubElement(record, 'total')
+		tipo_cambio = SubElement(record, 'tipo_cambio')
+		importe_depositado_dolares = SubElement(record, 'importe_depositado_dolares')
+		importe_letras = SubElement(record, 'importe_letras')
+		ingresos = SubElement(record, 'ingresos')
+		ingreso = SubElement(ingresos, 'ingreso')
+		concepto = SubElement(ingreso, 'concepto')
+		monto = SubElement(ingreso, 'monto')
 
-		for payslip in payslips:
-			dias_no_laborados += int(payslip.worked_days_line_ids.search([('code', '=', planilla_ajustes.cod_dias_no_laborados.codigo if planilla_ajustes else ''),
-																		('payslip_id', '=', payslip.id)], limit=1).number_of_days)
-		for payslip in payslips:
-			if not payslip.contract_id.hourly_worker:
-				dias_laborados += int(payslip.worked_days_line_ids.search([('code', '=', planilla_ajustes.cod_dias_laborados.codigo if len(planilla_ajustes) > 0 else ''),
-																	('payslip_id', '=', payslip.id)], limit=1).number_of_days)
-		dias_laborados=dias_laborados-self.feriados if dias_laborados > 0 else 0
-		if not planilla_ajustes.cod_dias_subsidiados:
-			raise UserError('Falta configurar codigos de dias subsidiados en Parametros de Boleta.')
-		wd_codes = planilla_ajustes.cod_dias_subsidiados.mapped('codigo')
-		dias_subsidiados = 0
-		for payslip in payslips:
-			wds = filter(lambda l:l.code in wd_codes and l.payslip_id == payslip,payslip.worked_days_line_ids)
-			dias_subsidiados += sum([int(i.number_of_days) for i in wds])
+		ciudad_fecha.text = "LIMA, 13 de Noviembre de 2018"
+		datos_empleador.text = "PAINO SCARPATI JOSE ALFREDO"
+		de.text = "46969060"
+		nombres_apellidos.text = self.employee_id.name
+		cargo.text = self.contract_id.job_id.name
+		fecha_ingreso.text = self.employee_id.date_entry_bl
+		entidad_depositaria.text = "FINANCIERA TFC"
+		cuenta_dolares.text = "143004200137639"
+		periodo_deposito_liquidar_del.text = "2018-05-01"
+		periodo_deposito_liquidar_al.text = "2018-10-31"
+		periodo_depositar_del.text = "2018-05-01"
+		periodo_depositar_al.text = "2018-10-31"
+		mes.text = "6"
+		dia.text = "0"
+		calculo_meses_laborados.text = "1400.00"
+		calculo_dias_laborados.text = "0.00"
+		importe_calculado.text = "1400.00"
+		total.text = "2800.00"
+		tipo_cambio.text = "3.3610"
+		importe_depositado_dolares.text = "416.54"
+		importe_letras.text = "CUATROCIENTOS DIEC Y SEIS Y 54/100 DOLARES AMERICANOS"
+		concepto.text = "SEULDO BASICO"
+		monto.text = "2400.00"
 
-		query_horas_sobretiempo = '''
-		select sum(number_of_days) as dias ,sum(number_of_hours) as horas ,sum(minutos) as minutos from hr_payslip_worked_days
-		where (code = 'HE25' OR code = 'HE35' or code = 'HE100')
-		and payslip_id in (%s)
-		''' % (','.join(str(i) for i in payslips.mapped('id')))
-
-		self.env.cr.execute(query_horas_sobretiempo)
-		# str(int(self.env.cr.dictfetchone()['horas']))
-		total_sobretiempo = self.env.cr.dictfetchone()
-		for payslip in payslips:
-			dias_faltas += self.env['hr.payslip.worked_days'].search([('code', '=', planilla_ajustes.cod_dias_no_laborados.codigo if planilla_ajustes else ''),
-																	('payslip_id', '=', payslip.id)], limit=1).number_of_days
-		if self.employee_id.calendar_id:
-			total = self.employee_id.calendar_id.average_hours if self.employee_id.calendar_id.average_hours > 0 else 8
-		else:
-			total = 8
-			#raise UserError(u'Este empleado no tiene un Horario establecido.')
-		total_horas_jornada_ordinaria = 0
-		for payslip in payslips:
-			if payslip.contract_id.hourly_worker:
-				total_horas_jornada_ordinaria += sum(payslip.worked_days_line_ids.filtered(lambda l:l.code == planilla_ajustes.cod_dias_laborados.codigo).mapped('number_of_hours'))
-
-		if self.employee_id.calendar_id:
-			total = self.employee_id.calendar_id.average_hours if self.employee_id.calendar_id.average_hours > 0 else 8
-		else:
-			total = 8
-		# formula para los dias laborados segun sunat
-		total_horas_minutos = modf(int(dias_laborados-dias_faltas)*total) if total_horas_jornada_ordinaria == 0 else total_horas_jornada_ordinaria
-		total_horas_jornada_ordinaria = total_horas_minutos[1]
-		total_minutos_jornada_ordinaria = Decimal(str(total_horas_minutos[0] * 60)).quantize(Decimal('1.'), rounding=ROUND_HALF_UP)
-		# busco cualquier campo ya que lo unico que quiero es usar la funcionalidad de la generacion de la boleta
-		payslip_run = self.env['hr.payslip.run']
-
-		payslip_run.genera_boleta_empleado(self.date_from, self.date_to, payslips, str(dias_no_laborados), str(int(dias_laborados - dias_faltas)), str(total_horas_jornada_ordinaria), str(total_minutos_jornada_ordinaria), (total_sobretiempo), str(dias_subsidiados), elements,
-										   company, categories, planilla_ajustes)
-
-
-		template_values = []
-		content = self.env.ref('requirements_paino.account_invoice_facturx_export').render(template_values)
-		content = b"<?xml version='1.0' encoding='UTF-8'?>" + content
-
-		import os
-		cwd = os.getcwd()
-		dir_path = os.path.dirname(os.path.realpath(__file__))
-		_logger.info(cwd)
-		_logger.info(dir_path)
-		tree = ElementTree()
-		tree.parse("/mnt/extra-addons2/requirements_paino/data/planilla_tmp.xml")
-
-		_logger.info(tree)
-		_logger.info('paso tree')
-		# tree.write('output.xml')
-		# archivo_pdf.build(content)
-
-		import sys
-		reload(sys)
-		sys.setdefaultencoding('iso-8859-1')
-		import os
 		vals = {
 			'output_name': 'output.xml',
-			'output_file': open("/mnt/extra-addons2/requirements_paino/data/planilla_tmp.xml", "rb").read().encode("base64"),
+			'output_file': tostring(records).encode("base64"),
 		}
 		sfs_id = self.env['planilla.export.file'].create(vals)
 		return {
